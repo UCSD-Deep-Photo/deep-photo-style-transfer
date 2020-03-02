5a0acac66c22e791a29ffa8b73cbc809005255f6
lrohrer <lrohrer@lrohrer-7523.pods.lrohrer.svc.cluster.local>
2020-03-02 12:35:39 -0800

    more examples

--- neuralnet/train.py
+++ neuralnet/train.py
@@ -28,14 +28,14 @@ def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta
     Init content features
     """
     logging.info('Initializing Content Features.')
-    showImage(content_img, 'Content Image', (timestamp + '__img_content'))
+    #showImage(content_img, 'Content Image', (timestamp + '__img_content'))
     _, _ = model(content_img,img_type='content')
 
     """
     Init style features 
     """
     logging.info('Initializing Style Features.')
-    showImage(style_img, 'Style Image', (timestamp + '__img_style'))
+    #showImage(style_img, 'Style Image', (timestamp + '__img_style'))
     _, _ = model(style_img,img_type='style')
     
     """
@@ -49,7 +49,7 @@ def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta
     for epoch in range(1,epochs+1):
         if (epoch % 100) == 0 or (epoch == 1):
             padded_epoch = '{0:04}'.format(epoch)
-            showImage(generated_img,'Generated Image',(timestamp + '_' + save_file + '_e' + str(padded_epoch))) #save img for first and every 100 epochs
+            #showImage(generated_img,'Generated Image',(timestamp + '_' + save_file + '_e' + str(padded_epoch))) #save img for first and every 100 epochs
 
         if (epoch % 10 == 0) or (epoch == 1):
             img_progress.append(unNormalize(generated_img[0].detach().cpu()))
