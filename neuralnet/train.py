f5890f99d687005e51f53d8a941bcd9ce17e2e98
lrohrer <lrohrer@lrohrer-4642.pods.lrohrer.svc.cluster.local>
2020-03-01 16:22:02 -0800

    added Luke images and animation code

--- neuralnet/train.py
+++ neuralnet/train.py
@@ -3,15 +3,18 @@ import time
 import torch
 import logging
 import torch.optim as optim
-from neuralnet.utils import showImage, checkGPU
+from neuralnet.utils import showImage, checkGPU, animate_progress, unNormalize
 
-def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta=0.01,  lr=0.05, epochs=1000,early_stop=5,timestamp=''):
+def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta=0.01,  lr=0.05, epochs=100,early_stop=5,timestamp=''):
     use_gpu      = next(model.parameters()).is_cuda
     result       = []
     ts           = time.time()
     train_loss   = 0.0
     counter      = 0
     model.train()
+    
+    img_progress = []
+    
     # TODO: ADD EARLY STOPPING AND SAVE BEST IMAGE
     # TODO: ADD LOSS PLOTS
 
@@ -40,10 +43,15 @@ def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta
     logging.info('Generating Image.')
     optimizer = optim.Adam([generated_img.requires_grad_(True)], lr=lr)
     
+#     img_progress.append(content_img[0].detach().cpu())
+    
     for epoch in range(1,epochs+1):
         if (epoch % 100) == 0 or (epoch == 1):
             padded_epoch = '{0:04}'.format(epoch)
             showImage(generated_img,'Generated Image',(timestamp + '_' + save_file + '_e' + str(padded_epoch))) #save img for first and every 100 epochs
+
+        if (epoch % 10 == 0) or (epoch == 1):
+            img_progress.append(unNormalize(generated_img[0].detach().cpu()))
 
         optimizer.zero_grad()    
         s_loss, c_loss = model(generated_img,img_type='generated')
@@ -59,5 +67,8 @@ def train(model, content_img, style_img, generated_img, save_file, alpha=5, beta
     result.append((train_loss))
     checkGPU()
     logging.info('Final Loss: {}'.format(loss.item()))
+    
+    animate_progress(img_progress, save_file+'_animated')
+    
     return result
 
--- neuralnet/utils.py
+++ neuralnet/utils.py
@@ -6,6 +6,10 @@ from datetime import datetime
 from torchvision import transforms
 from matplotlib import pyplot as plt
 
+import numpy as np
+import matplotlib.animation as animation
+from IPython.display import HTML
+
 def checkGPU():
     logging.info("GPU Usage - cached:{}/{} GB, allocated:{}/{} GB".format(round(torch.cuda.memory_cached()/1024/1024/1024,5), 
                 round(torch.cuda.max_memory_cached()/1024/1024/1024,5), 
@@ -32,3 +36,18 @@ def unNormalize(x):
     std = torch.tensor([0.229, 0.224, 0.225])
     unnorm = transforms.Normalize((-mean / std),(1.0 / std)) # ImageNet mean and std
     return unnorm(x)
+
+def animate_progress(img_progress, save_file): 
+    
+    # Set up formatting for the movie files
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=1800)
+    
+    fig = plt.figure(figsize=(10,10))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_progress]
+    ani = animation.ArtistAnimation(fig, ims, blit=True)
+
+    ani.save('out/' + save_file + '.mp4', writer=writer)
+    
+    HTML(ani.to_jshtml())
