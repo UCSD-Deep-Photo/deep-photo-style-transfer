780cab9a6c3113ca8611501c8739366e067a59bd
Bill Hogan <whogan@admins-MacBook-Pro-3.local>
2020-03-04 14:12:05 -0800

    testing other models and model variations

--- neuralnet/models.py
+++ neuralnet/models.py
@@ -172,3 +172,356 @@ class TVLoss(nn.Module):
 
         tv_loss = torch.mean(tv_height + tv_width)
         return tv_loss
+
+class vgg19_new(nn.Module):
+    def __init__(self):
+        super(vgg19_new, self).__init__()
+
+        # Initialize model
+        self.model = models.vgg19(pretrained=True)
+
+        # Freeze the weights
+        for param in self.model.parameters():
+            param.requires_grad = False
+        
+        self.c_loss, self.c_layers = [],[]
+        self.s_loss, self.s_layers = [],[]
+
+        self.conv0 = nn.Sequential(
+            self.model.features[0], # conv2d
+            self.model.features[1], # relu
+        )
+
+        self.conv1 = nn.Sequential(
+            self.model.features[2], # conv2d
+            self.model.features[3], # relu
+            self.model.features[4], # maxpool
+            self.model.features[5], # conv2d
+            self.model.features[6], # relu
+        )
+        self.conv2 = nn.Sequential(
+            self.model.features[7], # conv2d
+            self.model.features[8], # relu
+            self.model.features[9], # maxpool
+            self.model.features[10], # conv2d  
+            self.model.features[11], # relu 
+        )
+        self.conv3 = nn.Sequential(
+            self.model.features[12], # conv2d  
+            self.model.features[13], # relu 
+            self.model.features[14], # conv2d  
+            self.model.features[15], # relu 
+            self.model.features[16], # conv2d  
+            self.model.features[17], # relu 
+            self.model.features[18], # maxpool
+            self.model.features[19], # conv2d 
+            self.model.features[20], # relu
+        )
+        self.conv4a = nn.Sequential(
+            self.model.features[21],  # conv2d 
+            self.model.features[22], # relu
+        )
+        self.conv4b = nn.Sequential(
+            self.model.features[23], # conv2d 
+            self.model.features[24], # relu
+            self.model.features[25], # conv2d 
+            self.model.features[26], # relu
+            self.model.features[27], # maxpool
+            self.model.features[28], # conv2d 
+        )
+        self.conv5 = nn.Sequential(
+            self.model.features[29], # relu
+            self.model.features[30], # conv2d 
+            self.model.features[31], # relu
+            self.model.features[32], # conv2d 
+            self.model.features[33], # relu
+            self.model.features[34], # conv2d 
+            self.model.features[35], # relu
+            self.model.features[36], # maxpool
+        )
+        self.final = nn.Sequential(
+            self.model.avgpool,
+            self.model.classifier
+        )
+
+    def __call__(self, x, img_type):
+        ''' Make Model callable with default set to forward()'''
+        return self.forward(x, img_type)
+
+    def encoder(self, x, img_type='generated'):
+        # Reset loss for each fwd pass:
+        self.s_loss = []
+        self.c_loss = []
+
+        # first conv layer
+        x = self.conv0(x)
+
+        # insert style layer @ conv_1
+        layer = 0 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv1(x)
+
+        # insert style layer @ conv_2
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv2(x)
+        
+        # insert style layer @ conv_3
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv3(x)
+
+        # insert style layer @ conv_4
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv4a(x)
+
+        # insert content layer @ conv_4_2
+        if img_type == 'content': self.c_layers.append(ContentLayer(x))
+        elif img_type == 'generated': 
+            x = self.c_layers[0].forward(x)
+            self.c_loss.append(self.c_layers[0].loss)
+        x = self.conv4b(x)
+
+        # insert style layer @ conv_5
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv5(x)
+
+        return sum(self.s_loss), sum(self.c_loss)
+
+    def forward(self, x,img_type):
+        s_loss, c_loss = self.encoder(x,img_type)
+        return s_loss,c_loss
+
+class vgg19_new_fc(nn.Module):
+    def __init__(self):
+        super(vgg19_new_fc, self).__init__()
+
+        # Initialize model
+        self.model = models.vgg19(pretrained=True)
+
+        # Freeze the weights
+        for param in self.model.parameters():
+            param.requires_grad = False
+        
+        self.c_loss, self.c_layers = [],[]
+        self.s_loss, self.s_layers = [],[]
+
+        self.conv0 = nn.Sequential(
+            self.model.features[0], # conv2d
+            self.model.features[1], # relu
+        )
+
+        self.conv1 = nn.Sequential(
+            self.model.features[2], # conv2d
+            self.model.features[3], # relu
+            self.model.features[4], # maxpool
+            self.model.features[5], # conv2d
+            self.model.features[6], # relu
+        )
+        self.conv2 = nn.Sequential(
+            self.model.features[7], # conv2d
+            self.model.features[8], # relu
+            self.model.features[9], # maxpool
+            self.model.features[10], # conv2d  
+            self.model.features[11], # relu 
+        )
+        self.conv3 = nn.Sequential(
+            self.model.features[12], # conv2d  
+            self.model.features[13], # relu 
+            self.model.features[14], # conv2d  
+            self.model.features[15], # relu 
+            self.model.features[16], # conv2d  
+            self.model.features[17], # relu 
+            self.model.features[18], # maxpool
+            self.model.features[19], # conv2d 
+            self.model.features[20], # relu
+        )
+        self.conv4a = nn.Sequential(
+            self.model.features[21],  # conv2d 
+            self.model.features[22], # relu
+        )
+        self.conv4b = nn.Sequential(
+            self.model.features[23], # conv2d 
+            self.model.features[24], # relu
+            self.model.features[25], # conv2d 
+            self.model.features[26], # relu
+            self.model.features[27], # maxpool
+            self.model.features[28], # conv2d 
+        )
+        self.conv5 = nn.Sequential(
+            self.model.features[29], # relu
+            self.model.features[30], # conv2d 
+            self.model.features[31], # relu
+            self.model.features[32], # conv2d 
+            self.model.features[33], # relu
+            self.model.features[34], # conv2d 
+            self.model.features[35], # relu
+            self.model.features[36], # maxpool
+        )
+        self.final = nn.Sequential(
+            self.model.avgpool,
+            self.model.classifier
+        )
+
+    def __call__(self, x, img_type):
+        ''' Make Model callable with default set to forward()'''
+        return self.forward(x, img_type)
+
+    def encoder(self, x, img_type='generated'):
+        # Reset loss for each fwd pass:
+        self.s_loss = []
+        self.c_loss = []
+
+        # first conv layer
+        x = self.conv0(x)
+
+        # insert style layer @ conv_1
+        layer = 0 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv1(x)
+
+        # insert style layer @ conv_2
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv2(x)
+        
+        # insert style layer @ conv_3
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv3(x)
+
+        # insert style layer @ conv_4
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv4a(x)
+
+        # insert content layer @ conv_4_2
+        if img_type == 'content': self.c_layers.append(ContentLayer(x))
+        elif img_type == 'generated': 
+            x = self.c_layers[0].forward(x)
+            self.c_loss.append(self.c_layers[0].loss)
+        x = self.conv4b(x)
+
+        # insert style layer @ conv_5
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv5(x)
+
+        return sum(self.s_loss), sum(self.c_loss)
+
+    def forward(self, x,img_type):
+        s_loss, c_loss = self.encoder(x,img_type)
+        return s_loss,c_loss
+
+class resnet152(nn.Module):
+    def __init__(self):
+        super(resnet152, self).__init__()
+
+        # Initialize model
+        self.model = models.resnet152(pretrained=True)
+
+        # Freeze the weights
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.c_loss, self.c_layers = [],[]
+        self.s_loss, self.s_layers = [],[]
+
+        self.conv0 = nn.Sequential(
+            self.model.conv1,
+            self.model.bn1,
+            self.model.relu
+        )
+        self.conv1 = nn.Sequential(
+            self.model.maxpool,
+            self.model.layer1
+        )
+        self.conv2 = self.model.layer2
+        self.conv3 = self.model.layer3
+        self.conv4 = self.model.layer4
+
+
+    def __call__(self, x, img_type):
+        ''' Make Model callable with default set to forward()'''
+        return self.forward(x, img_type)
+
+    def encoder(self, x, img_type='generated'):
+        # Reset loss for each fwd pass:
+        self.s_loss = []
+        self.c_loss = []
+
+        # first conv layer
+        x = self.conv0(x)
+
+        # insert style layer @ conv_1
+        layer = 0 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv1(x)
+
+        # insert style layer @ conv_2
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv2(x)
+        
+        # insert style layer @ conv_3
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'generated': 
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv3(x)
+
+        # insert style layer @ conv_4
+        layer += 1 
+        if img_type == 'style': self.s_layers.append(StyleLayer(x))
+        elif img_type == 'content': self.c_layers.append(ContentLayer(x))
+        elif img_type == 'generated': 
+            x = self.c_layers[0].forward(x)
+            self.c_loss.append(self.c_layers[0].loss)
+            x = self.s_layers[layer].forward(x)
+            self.s_loss.append(self.s_layers[layer].loss)
+        x = self.conv4(x)
+
+        return sum(self.s_loss), sum(self.c_loss)
+
+    def forward(self, x,img_type):
+        s_loss, c_loss = self.encoder(x,img_type)
+        return s_loss,c_loss
