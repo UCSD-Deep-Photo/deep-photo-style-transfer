f3c656ed88f3a096a3818143e4276ff73ce69c1c
kexiang feng <k1feng@dsmlp-login.ucsd.edu>
2020-03-05 17:44:33 -0800

    add munit model

--- gans/util/util.py
+++ gans/util/util.py
@@ -40,8 +40,8 @@ def diagnose_network(net, name='network'):
             count += 1
     if count > 0:
         mean = mean / count
-    print(name)
-    print(mean)
+    logging.info(name)
+    logging.info(mean)
 
 
 def save_image(image_numpy, image_path, aspect_ratio=1.0):
@@ -69,10 +69,10 @@ def print_numpy(x, val=True, shp=False):
     """
     x = x.astype(np.float64)
     if shp:
-        print('shape,', x.shape)
+        logging.info('shape,', x.shape)
     if val:
         x = x.flatten()
-        print('mean = %3.3f, min = %3.3f, max = %3.3f, median = %3.3f, std=%3.3f' % (
+        logging.info('mean = %3.3f, min = %3.3f, max = %3.3f, median = %3.3f, std=%3.3f' % (
             np.mean(x), np.min(x), np.max(x), np.median(x), np.std(x)))
 
 
